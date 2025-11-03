d6f9a582435b50b68514c62f39366b91f54e69d3
RedDunadan <105070027+RedDunadan@users.noreply.github.com>
2025-11-03 16:43:17 -0300

    Empezando el proyecto

---
+++ Proyecto Final/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import notFound from './src/middlewares/notFound.js';
+
+const app = express();
+const PORT = 3000;
+
+
+app.use((req, res, next) => {
+    res.json({ message: 'Middleware active' });
+    console.log(req.method, req.url);
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use(notFound);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('500 Server Error');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
